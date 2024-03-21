f2454f8a47c0b82a3c5f9eb1be0314c23ba962da
daocongtuyen2x <daocongtuyen2x@gmail.com>
2024-03-21 20:44:25 +0700

    first commit

--- app.py
+++ app.py
@@ -99,15 +99,15 @@ def infer_image(img, size=None):
     return image
 
 
-@st.experimental_singleton
+@st.cache_resource 
 def load_model(path, device):
-    model_ = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
+    model_ = torch.hub.load('ultralytics/yolov5', 'yolov5s')
     model_.to(device)
     print("model to ", device)
     return model_
 
 
-@st.experimental_singleton
+@st.cache_resource
 def download_model(url):
     model_file = wget.download(url, out="models")
     return model_file
